60aac66fa490140b99a515152e406f0b23720b3b
Koen <koen.deboiserie@student.arteveldehs.be>
2016-10-31 11:03:16 +0100

    Update productiedossier

--- lectures/04/www-itunes/js/main.js
+++ lectures/04/www-itunes/js/main.js
@@ -2,65 +2,40 @@
   
   function ITunesApp() {
 
-    // Use Yahoo as a reverse proxy solve CORS (Cross Origin Resource Sharing problems)
-    this.API_URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22https%3A%2F%2Fitunes.apple.com%2Fsearch%3Fterm%3Ddaft%2Bpunk%22&format=json&diagnostics=true&callback=';
+    // URL of the Search API
+    this.API_URL = 'https://itunes.apple.com/search?term=daft+punk&entity=album';
+    // The results within the JSON-object
     this.results;
+
+     // Initialize App
+    this.init = function() {
+      console.log('1. Initialize the app');
+      this.loadData();
+    }
 
     // Load the data from the API (iTunes)
     this.loadData = function() {
-      // Hack
-      var that = this;
-      // Define a XMLHttpRequest object in order to load data
-      var xhr = new XMLHttpRequest();
-      // 1. Open a connection to the API
-      // get verb: Get the information from the end-point (READ execution)
-      // Third option means asynchronous action or not
-      xhr.open('get', this.API_URL, true);
-      // 2. Settings
-      xhr.responseType = 'json';
-      // 3. Listeners
-      // 3.1. onload: i received something that's not an error
-      xhr.onload = function() {
-        // Get the loaded data
-        var data = (!xhr.responseType)?JSON.parse(xhr.response):xhr.response;
-        // Get the real results from iTunes
-        this.results = data.query.results.json.results;
-        // Call the updateUI() function
-        that.updateUI();
-      };
-      // 3.2. onload: i received an error
-      xhr.onerror = function() {
-        console.log('Error');
-      };
-      // 4. Send the request
-      xhr.send();
+      console.log('2. Load the Data');
+      this.updateUI();
     };
 
+    // Update the User Interface (UI)
     this.updateUI = function() {
-      console.log('UPDATE THE UI DUDE');
-    }
+      console.log('3. Update UI');
+      this.generateTableUI();
+    };
+
+     // Generate the albums as a table with rows
+    this.generateTableUI = function() {
+      console.log('4. Generate UI with table-element');
+    };
 
   };
 
   // Make an instance of the ITunesApp
   var app = new ITunesApp();
-  app.loadData();
-
-this.updateUI = function(){
-
-    var tempStr = '';
-    tempStr += '<table classic="mdl-data-table mdl-js-data-table mdl-data-table-selectable-mdl-shadow--2dp">'
-    tempStr += '<thead>';
-    tempStr += '<tr>';
-    tempStr += '<th class="mdl-data-table__cell-non-numeric">Album</th>';
-    tempStr += '<th></th>';
-    tempStr += '<th>Artist</th>';
-    tempStr += '<th>Genre</th>';
-    tempStr += '<th>Price</th>';
-    tempStr += '</tr>';
-    for(var i=0;i<this.results.length;i++)
-}
+  // Initialize the app
+  app.init();
 
 
-
-})();
+    })();
